26226df2ec4456147ed68f090b5da60930e46b86
Donal Byrne <donalb@pagero.com>
2016-01-12 16:44:55 +0100

    Added comment to discovery cfg

--- discovery/capitan.cfg.sh
+++ discovery/capitan.cfg.sh
@@ -7,8 +7,10 @@ consul_ip="$(docker-machine ip swarm-consul)"
 docker_bridge_ip=172.17.0.1
 node_ip=0.0.0.0
 
-# Service Discovery - Consul
 cat <<EOF
+
+# Service Discovery - Consul
+
 consul-agent image progrium/consul
 consul-agent command -advertise $node_ip -join $consul_ip
 consul-agent hostname consul_agent
@@ -20,6 +22,7 @@ consul-agent publish ${node_ip}:8400:8400
 consul-agent publish ${node_ip}:8500:8500
 consul-agent publish ${docker_bridge_ip}:53:53
 consul-agent publish ${docker_bridge_ip}:53:53/udp
+# WILL ONLY RUN ON A NODE WHICH DOESN'T HAVE ONE ALREADY
 consul-agent env affinity:container!=~*-consul-agent_*
 consul-agent hook after.run $DIR/../wait_for_port.sh \$CAPITAN_CONTAINER_NAME 8500 30 sendify
 consul-agent hook after.start $DIR/../wait_for_port.sh \$CAPITAN_CONTAINER_NAME 8500 30 sendify
@@ -31,6 +34,7 @@ consul-agent net sendify
 registrator image gliderlabs/registrator:master
 registrator command -retry-interval 1000 -retry-attempts 5 -internal consul://${consul_ip}:8500
 registrator hostname registrator
+# WILL ONLY RUN ON A NODE WHICH DOESN'T HAVE ONE ALREADY
 registrator env affinity:container!=~*-registrator_*
 registrator volume /var/run/docker.sock:/tmp/docker.sock
 registrator scale $SCALE
